9a77b12b5cb45d40eb4ee8cdc665205baf2b2ac6
Motealieb <m.tealieb2014@gmail.com>
2026-05-23 14:34:03 +0300

    Initialize App component with routing setup

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
+function App(){
+
+  return(
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/profile/:id"
+         element={<Profile />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
